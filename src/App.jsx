35e06cc65d35141d0ef225af7ba542f7d99960d7
andrey <andrey_xoxlov_2000@mail.ru>
2024-03-26 22:23:01 +0300

    0.0.0.01

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,59 @@
+// App.jsx
 import './App.css';
-import phone from "./images/phone.png"
+import phone from "./images/phone.png";
+import magickstick from "./images/magicwand.png";
+import React, { useState } from 'react';
+import CasesInfoWrap from './components/CasesInfoWrap';
+import CarsContainer from './components/CarsContainer'
 
 function App() {
-  return (
 
+  const [selectedCase, setSelectedCase] = useState('На работу');
+  const handleCaseSelection = (caseName) => {
+    setSelectedCase(caseName);
+  };
+
+  return (
     <div className="MainScreenContainer">
       <div className="PromoContainer">
-          <div className="PromoContent">
-              <div className="Promo_Item_Left_Section">
-                <div className="Promo_Item_Left_Section_Title">
-                <h1>Свобода — когда своей машины нет. А каршеринг есть.</h1>
-                </div>
-                <button className="RegisterButton">
-                  <div className='RegisterButtonTitle'>
-                  Зарегистрироваться в Драйве
-                  </div>
-                  </button>
-                
+        <div className="PromoContent">
+          <div className="Promo_Item_Left_Section">
+            <div className="Promo_Item_Left_Section_Title">
+              <h1> Свобода — когда своей машины нет. А каршеринг есть.</h1>
             </div>
-            <div className="card_phone">
-                  <img className="phone-card" src={phone} alt={phone}/>
-                </div>
+            <button className="RegisterButton">
+              <div className='RegisterButtonTitle'>
+                Выбрать мечту
+              </div>
+            </button>
           </div>
-        
+          <div className="card_phone">
+            <img className="phone-card" src={phone} alt={phone} />
+          </div>
+        </div>
       </div>
+      <div className="Cases_Container">
+        <div className='Section_header'>
+          <img className='SectionIcon' src={magickstick} alt={magickstick}></img>
+          <h2> Наши машины для вашей жизни</h2>
+          <h3>Понажимайте, и увидите, какие автомобили по каким тарифам можно взять</h3>
+        </div>
+        <div className='Cases_Wrap'>
+        <button className={`Case_button ${selectedCase === 'На работу' ? 'SelectedButton' : ''}`} onClick={() => handleCaseSelection('На работу')}>На работу</button>
+        <button className={`Case_button ${selectedCase === 'В аэропорт' ? 'SelectedButton' : ''}`} onClick={() => handleCaseSelection('В аэропорт')}>В аэропорт</button>
+        <button className={`Case_button ${selectedCase === 'Переезд' ? 'SelectedButton' : ''}`} onClick={() => handleCaseSelection('Переезд')}>Переезд</button>
+        <button className={`Case_button ${selectedCase === 'С детьми на дачу' ? 'SelectedButton' : ''}`} onClick={() => handleCaseSelection('С детьми на дачу')}>С детьми на дачу</button> 
+        <button className={`Case_button ${selectedCase === 'Покататься' ? 'SelectedButton' : ''}`} onClick={() => handleCaseSelection('Покататься')}>Покататься</button> 
+        <button className={`Case_button ${selectedCase === 'На свидание' ? 'SelectedButton' : ''}`} onClick={() => handleCaseSelection('На свидание')}>На свидание</button> 
+        </div>
+        <CasesInfoWrap selectedCase={selectedCase} />
+      
+      </div>
+      <CarsContainer/>
+      
     </div>
+    
+    
   );
 }
 
---
+++ src/components/CarsContainer.jsx
@@ -0,0 +1,21 @@
+import icon from '../images/free-icon-car-wash-2211392.png'
+import './CarsContainer.css'
+
+function CarsContainer(){
+    return(
+        <div className="Cars_container">
+            <div className="Section_header">
+                <img className="SectionIcon" src={icon}></img>
+                <h2>Вот машины в вашем городе. В других — другие.</h2>
+            </div>
+            <div className='Cars_container_content'>
+                <div className='Cars_section_header_mobile'>
+                    <div className='Cars_section_header_title_mobile'>На каждый день</div>
+                    <div className='Cars_section_header_subtitle_mobile'>Нужны 21 год жизни и 2 года стажа</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CarsContainer;
